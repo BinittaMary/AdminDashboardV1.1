2183159a8abf9603cc58ce89a803b758e3ea4040
Binitta <binittamary.chandy@gmail.com>
2021-07-30 07:44:45 +0000

    first commit

---
+++ Admin-Dashboard-master/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,74 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { CoursesComponent } from './courses/courses.component';
+import { StaffsComponent } from './staffs/staffs.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ViewCourseComponent } from './view-course/view-course.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { EditCourseComponent } from './edit-course/edit-course.component';
+import { StaffFormComponent } from './staff-form/staff-form.component';
+import { TestimonialformComponent } from './testimonialform/testimonialform.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { AddStaffsComponent } from './add-staffs/add-staffs.component';
+import { EditStaffsComponent } from './edit-staffs/edit-staffs.component';
+import { ViewStaffsComponent } from './view-staffs/view-Staffs.component';
+
+const routes: Routes = [{
+  path: '',
+  component: PagesComponent,
+  children: [
+    {
+      path: 'staffs',
+      component: StaffsComponent,
+    },
+    {
+      path: 'addstaff',
+      component: AddStaffsComponent,
+    },
+    {
+      path: 'editstaff',
+      component:EditStaffsComponent,
+    },
+    {
+      path:'viewstaff',
+      component:ViewStaffsComponent,
+    },
+    {
+      path: 'courses',
+      component:CoursesComponent,
+    },
+    {
+      path: 'viewcourse',
+      component:ViewCourseComponent,
+    },
+    {
+      path: 'editcourse',
+      component:EditCourseComponent,
+    },
+    {
+      path: 'addcourse',
+      component:AddCourseComponent,
+    },
+    {
+      path: 'addtestimonial',
+      component:TestimonialformComponent,
+    },
+    {
+      path: 'testimonials',
+      component:TestimonialsComponent,
+    },
+    {
+      path: 'CourseRegistration',
+      component:UserRegistrationComponent,
+    },
+  ],
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {
+}
---
+++ Admin-Dashboard-master/src/app/pages/partnership-application/partnership-application.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-partnership-application',
+  templateUrl: './partnership-application.component.html',
+  styleUrls: ['./partnership-application.component.scss']
+})
+export class PartnershipApplicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
